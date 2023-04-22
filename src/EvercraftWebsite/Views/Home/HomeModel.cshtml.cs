6763309b68f7ff937775d8f8673858859b85c21a
CodeItQuick <evanontario@hotmail.com>
2023-04-22 20:51:24 -0300

    ✅ TCR - tests passing
    changed-lines:
        src: 13
        test: 0
    test-stats:
        run: 8
        passed: 8
        failed: 0
        skipped: 0
        error: 0
        duration: 2.403504s

--- src/EvercraftWebsite/Views/Home/HomeModel.cshtml.cs
+++ src/EvercraftWebsite/Views/Home/HomeModel.cshtml.cs
@@ -6,6 +6,10 @@ public class HomeModel : PageModel
 {
     private readonly ILogger<HomeModel> _logger;
 
+    public HomeModel()
+    {
+    }
+
     public HomeModel(ILogger<HomeModel> logger)
     {
         _logger = logger;
