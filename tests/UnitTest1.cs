8d8f91a15124ab37c9489e53eee52ded01616aa1
CodeItQuick <evanontario@hotmail.com>
2023-04-22 14:17:08 -0300

    Add First Controller Route

--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -1,16 +1,24 @@
 
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
 namespace tcr_evercraft_2_tests;
 
 public class Tests
 {
+    private HttpClient _client;
     [SetUp]
-    public void Setup(TestingWebAppFactory<Program> factory)
+    public void SetUp()
     {
+        var testingWebAppFactory = new TestingWebAppFactory<Program>();
+        _client = testingWebAppFactory.CreateClient();
     }
 
     [Test]
-    public void DummyTest()
+    public async Task DummyTest()
     {
-        Assert.Pass();
+        var response = await _client.GetAsync($"/Home/Index");
+
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
     }
 }
