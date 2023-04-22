5c090e12db1614cb6552b7e74089b68e27809bec
CodeItQuick <evanontario@hotmail.com>
2023-04-22 12:05:57 -0300

    first commit

---
+++ src/willThisRemain.cs
@@ -0,0 +1,10 @@
+public class HelloWorld
+{
+    public HelloWorld() {
+		Console.WriteLine("HelloWorld");
+		Console.WriteLine("this should be removed");
+		Console.WriteLine("I can do this though");
+    }
+}
+
+new HelloWorld();
---
+++ tests/UnitTest1.cs
@@ -0,0 +1,30 @@
+namespace tcr_evercraft_2_tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+    [Test]
+    public void TestFail()
+    {
+        Assert.Pass();
+    }
+    [Test]
+    public void TestPassesAgain()
+    {
+        Assert.Pass();
+    }
+    [Test]
+    public void TestFailsAgain()
+    {
+        Assert.Pass();
+    }
+}
