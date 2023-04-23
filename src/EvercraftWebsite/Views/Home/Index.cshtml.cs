aa5f339a818b6d8f0aa5cdb85f8508a0b653509f
CodeItQuick <evanontario@hotmail.com>
2023-04-23 00:43:07 -0300

    display saved username

--- src/EvercraftWebsite/Views/Home/Index.cshtml.cs
+++ src/EvercraftWebsite/Views/Home/Index.cshtml.cs
@@ -6,21 +6,6 @@ namespace EvercraftWebsite.Views.Home;
 
 public class HomeModel
 {
-    private readonly ILogger<HomeModel> _logger;
     public List<DnDCharacter>? DnDCharacters { get; set; } = new();
-
-
-    public HomeModel()
-    {
-    }
-
-    public HomeModel(ILogger<HomeModel> logger)
-    {
-        _logger = logger;
-    }
-
-    public void OnGet()
-    {
-
-    }
+    
 }
