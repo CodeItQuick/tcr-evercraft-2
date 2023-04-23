93bf0270307d7cf78255effb3a8f8b77c22ae8e0
CodeItQuick <evanontario@hotmail.com>
2023-04-22 21:06:57 -0300

    Revert "❌ TCR - tests failing"
    This reverts commit 1371ab37381ec8b6c830f87e35935bf42baa74c8.

--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -90,7 +90,7 @@ public class Tests
         Assert.IsNotNull(viewResult);
     }
     [Test]
-    public async Task CanRetrieveNewCharacterFromIndexes()
+    public async Task CanRetrieveNewCharacterFromIndex()
     {
         var homeController = new HomeController(new DbContextOptionsBuilder<EvercraftDbContext>()
             .UseInMemoryDatabase("TemporaryDatabase").Options);
@@ -100,6 +100,6 @@ public class Tests
         var viewResultModel = viewResult?.Model as HomeModel;
 
         Assert.IsNotNull(viewResult);
-        Assert.AreEqual(1, viewResultModel?.DnDCharacters?.Count);
+        // Assert.AreEqual(1, viewResultModel?.DnDCharacters?.Count);
     }
 }
