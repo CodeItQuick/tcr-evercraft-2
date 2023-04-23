ecbf68162741ccce7d509751b3089b9ab61a4f2c
CodeItQuick <evanontario@hotmail.com>
2023-04-22 21:35:43 -0300

    ❌ TCR - tests failing
    changed-lines:
        src: 6
        test: 0
    test-stats:
        run: 9
        passed: 8
        failed: 1
        skipped: 0
        error: 0
        duration: 4.898975s

--- src/EvercraftWebsite/Views/Home/Index.cshtml.cs
+++ src/EvercraftWebsite/Views/Home/Index.cshtml.cs
@@ -1,13 +1,17 @@
 using EvercraftWebsite.Controllers;
 using EvercraftWebsite.Data;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
 
 namespace EvercraftWebsite.Views.Home;
 
+[JsonObject(MemberSerialization = MemberSerialization.OptIn)]
 public class HomeModel : PageModel
 {
     private readonly ILogger<HomeModel> _logger;
-    public List<DnDCharacter>? DnDCharacters { get; set; }
+
+    [JsonProperty(PropertyName = "DnDCharacters")]
+    public List<DnDCharacter>? DnDCharacters { get; set; } = new List<DnDCharacter>();
 
 
     public HomeModel()
