663a8081174154b3cccb81e02177a5f182aafd6b
CodeItQuick <evanontario@hotmail.com>
2023-04-22 21:35:43 -0300

    Revert "❌ TCR - tests failing"
    This reverts commit ecbf68162741ccce7d509751b3089b9ab61a4f2c.

--- src/EvercraftWebsite/Views/Home/Index.cshtml.cs
+++ src/EvercraftWebsite/Views/Home/Index.cshtml.cs
@@ -1,17 +1,13 @@
 using EvercraftWebsite.Controllers;
 using EvercraftWebsite.Data;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Newtonsoft.Json;
 
 namespace EvercraftWebsite.Views.Home;
 
-[JsonObject(MemberSerialization = MemberSerialization.OptIn)]
 public class HomeModel : PageModel
 {
     private readonly ILogger<HomeModel> _logger;
-
-    [JsonProperty(PropertyName = "DnDCharacters")]
-    public List<DnDCharacter>? DnDCharacters { get; set; } = new List<DnDCharacter>();
+    public List<DnDCharacter>? DnDCharacters { get; set; }
 
 
     public HomeModel()
