82487ba234c21e5837aed4a2e46bc86df660d717
CodeItQuick <evanontario@hotmail.com>
2023-04-22 21:46:11 -0300

    ❌ TCR - tests failing
    changed-lines:
        src: 8
        test: 0
    test-stats:
        run: 9
        passed: 7
        failed: 2
        skipped: 0
        error: 0
        duration: 2.648859s

--- src/EvercraftWebsite/Program.cs
+++ src/EvercraftWebsite/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
+builder.Services.AddDbContext<EvercraftDbContext>();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddRazorPages();
