67db71721cf13087890ca3157205a2b5de81ec60
CodeItQuick <evanontario@hotmail.com>
2023-04-22 21:13:33 -0300

    ✅ TCR - tests passing
    changed-lines:
        src: 0
        test: 6
    test-stats:
        run: 9
        passed: 9
        failed: 0
        skipped: 0
        error: 0
        duration: 2.443544s

--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -90,17 +90,17 @@ public class Tests
         Assert.IsNotNull(viewResult);
     }
     [Test]
-    public async Task CanRetrieveNewCharacterFromIndex()
+    public async Task CanRetrieveNewCharacterFromIndexes()
     {
         var homeController = new HomeController(new DbContextOptionsBuilder<EvercraftDbContext>()
             .UseInMemoryDatabase("TemporaryDatabase").Options);
 
-        homeController.Create("first character");
+        homeController.Create("create character test");
         var viewResult = homeController.Index() as ViewResult;
         var viewResultModel = viewResult?.Model as HomeModel;
 
         Assert.IsNotNull(viewResult);
         Assert.AreEqual(1, viewResultModel?.DnDCharacters?.Count);
-        Assert.AreEqual("first character", viewResultModel?.DnDCharacters?.Last().CharacterName);
+        Assert.AreEqual("create character test", viewResultModel?.DnDCharacters?.Last().CharacterName);
     }
 }
