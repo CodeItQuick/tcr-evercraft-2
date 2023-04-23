c46f9e5213884b06891d96fdc23c10e107d57b4b
CodeItQuick <evanontario@hotmail.com>
2023-04-23 10:53:50 -0300

    ❌ TCR - tests failing
    changed-lines:
        src: 0
        test: 1
    test-stats:
        run: 12
        passed: 11
        failed: 1
        skipped: 0
        error: 0
        duration: 2.709864s

--- tests/RepositoryTests.cs
+++ tests/RepositoryTests.cs
@@ -49,6 +49,7 @@ public class RepositoryTests
         
         
         Assert.That(createCharacter, Is.EqualTo(1));
+        Assert.AreEqual(0, evercraftDbContext.DnDCharacters.Count());
         return Task.CompletedTask;
     }
 }
