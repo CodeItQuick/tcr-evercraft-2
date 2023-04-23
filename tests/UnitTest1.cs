349bef11ac11ed23ff0f1e7bed7635d79baac944
CodeItQuick <evanontario@hotmail.com>
2023-04-22 21:00:08 -0300

    ❌ TCR - tests failing
    changed-lines:
        src: 0
        test: 3
    test-stats:
        run: 9
        passed: 8
        failed: 1
        skipped: 0
        error: 0
        duration: 2.585746s

--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Net;
 using EvercraftWebsite.Controllers;
 using EvercraftWebsite.Data;
+using EvercraftWebsite.Views.Home;
 using Microsoft.EntityFrameworkCore;
 
 namespace tcr_evercraft_2_tests;
@@ -96,7 +97,9 @@ public class Tests
 
         homeController.Create("first character");
         var viewResult = homeController.Index() as ViewResult;
-        
+        var viewResultModel = viewResult?.Model as HomeModel;
+
         Assert.IsNotNull(viewResult);
+        Assert.AreEqual(1, viewResultModel?.DnDCharacters?.Count);
     }
 }
