fcaf96ba53d346fde9601866978db91a421708c0
CodeItQuick <evanontario@hotmail.com>
2023-04-29 10:37:46 -0300

    ✅ TCR - tests passing
    changed-lines:
        src: 6
        test: 0
    test-stats:
        run: 22
        passed: 22
        failed: 0
        skipped: 0
        error: 0
        duration: 2.32624s

--- src/EvercraftWebsite/Controllers/DieRandomPicker.cs
+++ src/EvercraftWebsite/Controllers/DieRandomPicker.cs
@@ -5,6 +5,12 @@ public class DieRandomPicker : Random
     public override int Next(int minValue, int maxValue)
     {
         var baseRoll = base.Next(minValue, maxValue);
+        bool hasCustomRoll = true;
+        if (hasCustomRoll)
+        {
+            int customRoll = 11;
+            baseRoll = customRoll;
+        }
         return baseRoll;
     }
 }
