b04c45ab89a5ffe5de66c5fd0532979819de7a6f
CodeItQuick <evanontario@hotmail.com>
2023-04-29 19:17:00 -0300

    ✅ TCR - tests passing
    changed-lines:
        src: 0
        test: 2
    test-stats:
        run: 23
        passed: 23
        failed: 0
        skipped: 0
        error: 0
        duration: 2.085179s

--- tests/AdapterSmokeTests.cs
+++ tests/AdapterSmokeTests.cs
@@ -80,7 +80,7 @@ public class AdapterSmokeTests
     {
         var response = await _client.GetAsync($"/Home/Delete/1");
 
-        var redirectLocation = response!.Headers.Location;
+        var redirectLocation = response!.Headers.Location?.OriginalString;
         
         response.EnsureSuccessStatusCode();
         Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
