9e25b7687b7e3b43b0b682a03cf96a837a494231
CodeItQuick <evanontario@hotmail.com>
2023-04-29 10:33:05 -0300

    ✅ TCR - tests passing
    changed-lines:
        src: 4
        test: 0
    test-stats:
        run: 22
        passed: 22
        failed: 0
        skipped: 0
        error: 0
        duration: 3.001183s

--- src/EvercraftWebsite/Controllers/HomeController.cs
+++ src/EvercraftWebsite/Controllers/HomeController.cs
@@ -79,5 +79,9 @@ namespace EvercraftWebsite.Controllers
 
     public class DieRandomPicker : Random
     {
+        public override int Next(int minValue, int maxValue)
+        {
+            return base.Next(minValue, maxValue);
+        }
     }
 }
