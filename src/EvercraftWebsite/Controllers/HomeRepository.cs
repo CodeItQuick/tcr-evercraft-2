b3ad8f8038a4a91b8474083c4dd17fb5ee944a9f
CodeItQuick <evanontario@hotmail.com>
2023-05-06 19:25:40 -0300

    ✅ TCR - tests passing
    changed-lines:
        src: 2
        test: 0
    test-stats:
        run: 49
        passed: 49
        failed: 0
        skipped: 0
        error: 0
        duration: 2.965846s

--- src/EvercraftWebsite/Controllers/HomeRepository.cs
+++ src/EvercraftWebsite/Controllers/HomeRepository.cs
@@ -144,7 +144,7 @@ public class HomeRepository : IHomeRepository
         
         if (dnDCharacter is not { } character || character.Armor >= randomDieRoll) return;
 
-        var coreDamage = 1 + ModifierTable[(int) character.StrengthModifier];
+        var coreDamage = (int) character.StrengthModifier < 10 ? 1: 1 + ModifierTable[(int) character.StrengthModifier];
         var damageAmt = randomDieRoll == 20 ? 2 * coreDamage : coreDamage;
 
         if (character.HitPoints <= damageAmt)
