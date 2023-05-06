29b2146f4f571c734cf550253bb9f32a53ed3e5d
CodeItQuick <evanontario@hotmail.com>
2023-05-06 19:37:47 -0300

    ❌ TCR - tests failing
    changed-lines:
        src: 0
        test: 17
    test-stats:
        run: 51
        passed: 50
        failed: 1
        skipped: 0
        error: 0
        duration: 6.259451s

--- tests/RepositoryTests.cs
+++ tests/RepositoryTests.cs
@@ -464,4 +464,21 @@ public class RepositoryTests
         Assert.That(evercraftDbContext.DnDCharacters.First().HitPoints, Is.EqualTo(1));
         
     }
+    [Test]
+    public void RepositoryCanAttackNewCharactersAndOnModifierChangesArmorIsModified()
+    { 
+        var dbContextOptions = new DbContextOptionsBuilder<EvercraftDbContext>()
+            .UseInMemoryDatabase("ModifierChangesHitStrengthModifierDoublesHit").Options;
+        var evercraftDbContext = new EvercraftDbContext(dbContextOptions);
+        var homeRepository = new HomeRepository(
+            evercraftDbContext);
+        homeRepository.CreateCharacter("can attack character");
+        homeRepository.SetModifier(1, 10, "Dexterity");
+
+        homeRepository.AttackCharacter(1, 13);
+
+        Assert.That(evercraftDbContext.DnDCharacters.Count(), Is.EqualTo(1));
+        Assert.That(evercraftDbContext.DnDCharacters.First().HitPoints, Is.EqualTo(4));
+        
+    }
 }
