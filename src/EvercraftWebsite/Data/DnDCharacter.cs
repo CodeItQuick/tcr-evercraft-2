d5769f98a62c7568b74ec6bbcacd7d5e68cf1c1c
CodeItQuick <evanontario@hotmail.com>
2023-05-06 11:08:55 -0300

    ✅ TCR - tests passing
    changed-lines:
        src: 26
        test: 0
    test-stats:
        run: 32
        passed: 32
        failed: 0
        skipped: 0
        error: 0
        duration: 1.723318s

--- src/EvercraftWebsite/Data/DnDCharacter.cs
+++ src/EvercraftWebsite/Data/DnDCharacter.cs
@@ -12,6 +12,32 @@ public class DnDCharacter
     public int HitPoints { get; set; } = 5;
     public int Armor { get; set; } = 10;
     public CharacterAlignment Alignment { get; set; } = CharacterAlignment.Neutral;
+    public CharacterModifier StrengthModifier { get; set; }
+}
+
+public enum CharacterModifier
+{
+    One = -5,
+    Two = -4,
+    Three = -4,
+    Four = -3,
+    Five = -3,
+    Six = -2,
+    Seven = -2,
+    Eight = -1,
+    Nine = -1,
+    Ten = 0,
+    Eleven = 0,
+    Twelve = 1,
+    Thirteen = 1,
+    Fourteen = 2,
+    Fifteen = 2,
+    Sixteen = 3,
+    Seventeen = 3,
+    Eighteen = 4,
+    Nineteen = 4,
+    Twenty = 5,
+        
 }
 
 public enum CharacterAlignment
