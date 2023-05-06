5e208d34c7f7d1a2478cf554c266b6edba5f1d4f
CodeItQuick <evanontario@hotmail.com>
2023-05-06 10:41:22 -0300

    ✅ TCR - tests passing
    changed-lines:
        src: 9
        test: 0
    test-stats:
        run: 28
        passed: 28
        failed: 0
        skipped: 0
        error: 0
        duration: 1.54444s

--- src/EvercraftWebsite/Data/DnDCharacter.cs
+++ src/EvercraftWebsite/Data/DnDCharacter.cs
@@ -13,4 +13,13 @@ public class DnDCharacter
 
     public int HitPoints { get; set; } = 5;
     public int Armor { get; set; } = 10;
+
+    public CharacterAlignment Alignment { get; set; } = CharacterAlignment.Neutral;
+}
+
+public enum CharacterAlignment
+{
+    Evil = -1,
+    Neutral = 0,
+    Good = 1
 }
