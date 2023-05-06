ef4ef3cfe7b0f267ce1de213239b298c3fcc3217
CodeItQuick <evanontario@hotmail.com>
2023-05-06 19:28:33 -0300

    Revert "❌ TCR - tests failing"
    This reverts commit 19ef3666eb9834a612d9d16fd4dbe072505604b3.

--- tests/RepositoryTests.cs
+++ tests/RepositoryTests.cs
@@ -456,11 +456,11 @@ public class RepositoryTests
         var homeRepository = new HomeRepository(
             evercraftDbContext);
         homeRepository.CreateCharacter("can attack character");
-        homeRepository.SetModifier(1, 14, "Strength");
+        homeRepository.SetModifier(1, 4, "Strength");
 
-        homeRepository.AttackCharacter(1, 20);
+        homeRepository.AttackCharacter(1, 16);
 
-        Assert.That(evercraftDbContext.DnDCharacters.First().HitPoints, Is.EqualTo(1));
+        Assert.That(evercraftDbContext.DnDCharacters.First().HitPoints, Is.EqualTo(4));
         
     }
 }
