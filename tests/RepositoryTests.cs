454e8a922aa01c31b13a8d051ce4f34e2356b371
CodeItQuick <evanontario@hotmail.com>
2023-05-06 10:28:28 -0300

    Revert "❌ TCR - tests failing"
    This reverts commit fd70f45a1464f9171a3beb626cd29e342a32226d.

--- tests/RepositoryTests.cs
+++ tests/RepositoryTests.cs
@@ -160,8 +160,6 @@ public class RepositoryTests
         homeRepository.AttackCharacter(1, 11);
         homeRepository.AttackCharacter(1, 11);
         
-        homeRepository.AttackCharacter(1, 11);
-        
-        Assert.That(evercraftDbContext.DnDCharacters.Count(), Is.EqualTo(0));
+        Assert.That(evercraftDbContext.DnDCharacters.First().HitPoints, Is.EqualTo(1));
     }
 }
