caaa7ae241c92a5a45e04ee2fc4415c7195fda1e
CodeItQuick <evanontario@hotmail.com>
2023-05-07 11:29:54 -0300

    ✅ TCR - tests passing
    changed-lines:
        src: 3
        test: 0
    test-stats:
        run: 61
        passed: 61
        failed: 0
        skipped: 0
        error: 0
        duration: 2.21955s

--- src/EvercraftWebsite/Controllers/HomeRepository.cs
+++ src/EvercraftWebsite/Controllers/HomeRepository.cs
@@ -148,7 +148,8 @@ public class HomeRepository : IHomeRepository
 
         // weaker enemies get hit harder, stronger enemies only get hit for 1 damage
         var coreDamage = (int) character.StrengthModifier > 10 ? 1: 1 - ModifierTable[(int) character.StrengthModifier];
-        var damageAmt = randomDieRoll == 20 ? 2 * coreDamage : coreDamage;
+        var damageAmt = randomDieRoll == 20 ? 2 * coreDamage + character.ExperiencePoints / 1000 :
+            coreDamage + character.ExperiencePoints / 1000;
 
         var characterDied = character.HitPoints <= damageAmt;
         if (characterDied)
