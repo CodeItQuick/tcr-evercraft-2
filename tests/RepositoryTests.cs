77a42363d94895f67b6de03dd5bf413efc803d52
CodeItQuick <evanontario@hotmail.com>
2023-05-07 11:26:11 -0300

    ✅ TCR - tests passing
    changed-lines:
        src: 0
        test: 20
    test-stats:
        run: 61
        passed: 61
        failed: 0
        skipped: 0
        error: 0
        duration: 2.425188s

--- tests/RepositoryTests.cs
+++ tests/RepositoryTests.cs
@@ -637,4 +637,24 @@ public class RepositoryTests
         Assert.That(evercraftDbContext.DnDCharacters.Count(), Is.EqualTo(1));
         Assert.That(evercraftDbContext.DnDCharacters.Last().HitPoints, Is.EqualTo(14));
     }
+    [Test]
+    public void RepositoryCanAttackNewCharactersAndAtOneHitPointDiesAttackRollModifiedByOne()
+    { 
+        var dbContextOptions = new DbContextOptionsBuilder<EvercraftDbContext>()
+            .UseInMemoryDatabase("CanGainLevelsAttackRollModifiedByOne").Options;
+        var evercraftDbContext = new EvercraftDbContext(dbContextOptions);
+        var homeRepository = new HomeRepository(
+            evercraftDbContext);
+        homeRepository.CreateCharacter("can attack character");
+        homeRepository.CreateCharacter("Gained Experience Character");
+        homeRepository.SetModifier(2, 14, "Constitution");
+        homeRepository.AttackCharacter(1, 11);
+        homeRepository.AttackCharacter(1, 11);
+        homeRepository.AttackCharacter(1, 11);
+        homeRepository.AttackCharacter(1, 11);
+        
+        homeRepository.AttackCharacter(1, 11);
+
+        Assert.That(evercraftDbContext.DnDCharacters.Count(), Is.EqualTo(1));
+    }
 }
