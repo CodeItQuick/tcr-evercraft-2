063fee571272835cd2f5bcce65ff912a33cb92ec
CodeItQuick <evanontario@hotmail.com>
2023-05-07 11:04:17 -0300

    ❌ TCR - tests failing
    changed-lines:
        src: 0
        test: 34
    test-stats:
        run: 56
        passed: 55
        failed: 1
        skipped: 0
        error: 0
        duration: 2.331903s

--- tests/RepositoryTests.cs
+++ tests/RepositoryTests.cs
@@ -446,23 +446,23 @@ public class RepositoryTests
         Assert.That(evercraftDbContext.DnDCharacters.First().HitPoints, Is.EqualTo(4));
         
     }
-    // [Test]
-    // public void RepositoryCanAttackNewCharactersAndOnModifierChangesHitStrengthCriticalHitDoublesModifier()
-    // { 
-    //     var dbContextOptions = new DbContextOptionsBuilder<EvercraftDbContext>()
-    //         .UseInMemoryDatabase("ModifierChangesHitStrengthModifierDoublesHit").Options;
-    //     var evercraftDbContext = new EvercraftDbContext(dbContextOptions);
-    //     var homeRepository = new HomeRepository(
-    //         evercraftDbContext);
-    //     homeRepository.CreateCharacter("can attack character");
-    //     homeRepository.SetModifier(1, 13, "Strength");
-    //
-    //     homeRepository.AttackCharacter(1, 20);
-    //
-    //     Assert.That(evercraftDbContext.DnDCharacters.Count(), Is.EqualTo(1));
-    //     Assert.That(evercraftDbContext.DnDCharacters.First().HitPoints, Is.EqualTo(1));
-    //     
-    // }
+    [Test]
+    public void RepositoryCanAttackNewCharactersAndOnModifierChangesHitStrengthCriticalHitDoublesModifier()
+    { 
+        var dbContextOptions = new DbContextOptionsBuilder<EvercraftDbContext>()
+            .UseInMemoryDatabase("ModifierChangesHitStrengthModifierDoublesHit").Options;
+        var evercraftDbContext = new EvercraftDbContext(dbContextOptions);
+        var homeRepository = new HomeRepository(
+            evercraftDbContext);
+        homeRepository.CreateCharacter("can attack character");
+        homeRepository.SetModifier(1, 7, "Strength");
+    
+        homeRepository.AttackCharacter(1, 20);
+    
+        Assert.That(evercraftDbContext.DnDCharacters.Count(), Is.EqualTo(1));
+        Assert.That(evercraftDbContext.DnDCharacters.First().HitPoints, Is.EqualTo(1));
+        
+    }
     
     [Test]
     public void RepositoryCanAttackNewCharactersAndOnModifierChangesArmorAmount()
