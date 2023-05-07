be3c29107dff084aac7dbd8101cfba1ca6a3ecb4
CodeItQuick <evanontario@hotmail.com>
2023-05-07 11:22:54 -0300

    ✅ TCR - tests passing
    changed-lines:
        src: 2
        test: 2
    test-stats:
        run: 59
        passed: 59
        failed: 0
        skipped: 0
        error: 0
        duration: 2.378512s

--- tests/RepositoryTests.cs
+++ tests/RepositoryTests.cs
@@ -614,6 +614,6 @@ public class RepositoryTests
         homeRepository.AttackCharacter(1, 11);
 
         Assert.That(evercraftDbContext.DnDCharacters.Count(), Is.EqualTo(1));
-        Assert.That(evercraftDbContext.DnDCharacters.Last().HitPoints, Is.EqualTo(11));
+        Assert.That(evercraftDbContext.DnDCharacters.Last().HitPoints, Is.EqualTo(12));
     }
 }
